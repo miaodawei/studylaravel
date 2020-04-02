904a81471cf4f4192553afd2f01cebc81f5f5539
meiliwu_苗大伟 <miaodawei@meiliwu.com>
2020-04-02 23:47:54 +0800

    all

--- app/Services/Tests/Test.php
+++ app/Services/Tests/Test.php
@@ -7,7 +7,9 @@ namespace App\Services\Tests;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Swoole\Coroutine;
+use Swoole\ExitException;
 use function Co\run;
+use function foo\func;
 
 class Test
 {
@@ -21,22 +23,50 @@ class Test
 //        }
         $n = 0;
         $cid = Coroutine::getCid();
-        go(function() use ($n) {
-//            echo '  '.json_encode(Coroutine::stats())."\r\n";
-            for($i = 0; $i < 10; $i ++) {
-                DB::connection('mysql')->table('erp_user')->where('id', 64079)
-                    ->increment('company_id');
-//                sleep(1);
-//                for($j = 0; $j < 5; $j ++) {
+        $chan = new Coroutine\Channel(2);
+        go(function() use ($chan, $n) {
+            for($i = 0; $i < 5; $i ++) {
+                for($j = 0; $j < 5; $j ++) {
+                    sleep(1);
 //                    echo $n.'  '.Coroutine::getCid()."\r\n";
-////                    echo '  '.json_encode(Coroutine::stats())."\r\n";
-//                    $n ++;
-//                }
+                    $chan->push($n);
+                    $n ++;
+
+                }
             }
         });
-        echo ' 出来了... '.Coroutine::exists($cid).'  '.$cid."<br>";
-        DB::connection('mysql')->table('erp_user')->where('id', 64079)
-            ->update(['company_id' => 30]);
+        go(function() use ($chan) {
+            try {
+                while(true) {
+                    sleep(1);
+                    $nn = $chan->pop();
+//                    if(!$nn) {
+//                        exit;
+//                    }
+                    echo Coroutine::getCid().' # '.$nn." @ ".date('Ymd H:i:s')."\r\n";
+                }
+            }catch (ExitException $e) {
+                Log::error('报错了1111：'.$e->getMessage());
+            }
+        });
+//        go(function() use ($chan) {
+//            try {
+//                while(true) {
+//                    sleep(1);
+//                    $nn = $chan->pop();
+////                    if(!$nn) {
+////                        exit;
+////                    }
+//                    echo Coroutine::getCid().' # '.$nn." @ ".date('Ymd H:i:s')."\r\n";
+//                }
+//            }catch (ExitException $e) {
+//                Log::error('报错了2222：'.$e->getMessage());
+//            }
+//        });
+        echo ' 出来了1111... '.Coroutine::exists($cid).'  '.$cid."<br>";
+
+//        DB::connection('mysql')->table('erp_user')->where('id', 64079)
+//            ->update(['company_id' => 30]);
 //        echo '  '.json_encode(Coroutine::stats())."<br>";
 //        Log::info('aaaa');
 
